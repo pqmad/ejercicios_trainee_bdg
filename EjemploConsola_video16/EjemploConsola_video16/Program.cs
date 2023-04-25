d2a95f409b1f124779d25a5070ddfe861196ed8d
pqmad <3062932420313@ingenieria.usac.edu.gt>
2023-04-25 17:56:41 -0600

    Se agrego video 16 y 17

---
+++ EjemploConsola_video16/EjemploConsola_video16/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploConsola_video16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Holis");
+            Console.Read();
+        }
+    }
+}
