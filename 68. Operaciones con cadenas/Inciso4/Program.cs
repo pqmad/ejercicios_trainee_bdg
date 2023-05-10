33dbb2eaa9fb65f8106714c804d949a2c2932e12
Madeline Pérez <3062932420313@ingenieria.usac.edu.gt>
2023-05-10 02:07:18 -0600

    Se termino el ejercicio 68 y se actulizo planificacion

---
+++ 68. Operaciones con cadenas/Inciso4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Inciso4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var cadena= new StringBuilder();
+            var contador = 0;
+            while (contador<4)
+            {
+                WriteLine("Escribe un número:");
+                var numeroIngresado = ReadLine();
+                switch (contador)
+                {
+                    case 0:
+                        cadena.Append($"El primer numero introducido es el {numeroIngresado}, ");
+                        break;
+                    case 1:
+                        cadena.Append($"despues han introducido el {numeroIngresado} ");
+                        break;
+                    case 2:
+                        cadena.Append($"y {numeroIngresado} ");
+                        break;  
+                    case 3:
+                        cadena.Append($"y por ultimo el {numeroIngresado}");
+                        break;
+                }
+                
+                contador++;
+            }
+            WriteLine($"\n{cadena}");
+            ReadKey();
+        }
+    }
+}
---
+++ 68. Operaciones con cadenas/Inciso5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Inciso5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 90; i > 64; i--)
+            {
+                WriteLine("\t"+Convert.ToChar(i));
+            }
+            ReadKey();
+        }
+    }
+}
