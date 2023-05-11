71b5cba59309d28c2dadf049d45e0ef6b21255f3
Madeline Pérez <3062932420313@ingenieria.usac.edu.gt>
2023-05-10 19:47:21 -0600

    Reto semanal

---
+++ Semana 2/Reto Semana 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Reto_Semana_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Escribe una palabra:");
+            var palabra = ReadLine().ToLower();
+            if (PalabraInversa(palabra) ==palabra)
+            {
+                WriteLine("Si es palíndromo");
+            }
+            else
+            {
+                WriteLine("No es palíndromo");
+            }
+            ReadKey();
+        }
+        public static string PalabraInversa(string palabra)
+        {
+            var inversa = "";
+            for (int i = palabra.Length-1; i>=0; i--)
+            {
+                inversa+= palabra[i];
+            }
+            return inversa;
+        }
+    }
+}
---
+++ Semana 2/Reto Semana 2/README.md
@@ -0,0 +1,8 @@
+> 📌 **Tiempo**
+
+| Actividad | tiempo |
+| :--: | :--: |
+| Elaboración del programa | 4:25 minutos |
+| Probar el programa | 1:59 minutos |
+| **Total** | **6:24 minutos** |
+
