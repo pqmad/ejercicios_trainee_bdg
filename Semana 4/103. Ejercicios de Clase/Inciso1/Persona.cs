79d019a0f9c2a016d8711b64c42125dd5ec498e0
pqmad <3062932420313@ingenieria.usac.edu.gt>
2023-05-17 22:07:54 -0600

    Ejercicio 103

---
+++ Semana 4/103. Ejercicios de Clase/Inciso1/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Inciso1
+{
+	public class Persona
+	{
+		public string nombre { get; private set; }
+		public int edad { get; private set; }
+
+        public Persona()
+		{
+			nombre = "";
+			edad = 0;
+		}
+        public Persona(string Nombre, int Edad)
+        {
+            nombre = Nombre;
+            edad = Edad;
+        }
+    }
+}
+
---
+++ Semana 4/103. Ejercicios de Clase/Inciso1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+
+namespace Inciso1;
+class Program
+{
+    private static  List<Persona> ListadoPersonas = new List<Persona>();
+    static void Main(string[] args)
+    {
+        var cantidad = 0;
+        while (cantidad < 5)
+        {
+            WriteLine("Ingrese nombre:");
+            var nombre = ReadLine().ToString();
+            WriteLine($"Ingrese edad de {nombre}:");
+            var edad = Convert.ToInt32(ReadLine());
+
+            ListadoPersonas.Add(new Persona(nombre, edad));
+            cantidad++;
+        }
+        ImprimeMayorEdad();
+        ReadKey();
+    }
+
+    public static void ImprimeMayorEdad()
+    {
+        WriteLine("\n\n############## MAYORES DE EDAD ##############");
+        foreach (var persona in ListadoPersonas)
+        {
+            if(persona.edad >= 18){
+                WriteLine(persona.nombre);
+            }
+        }
+    }
+}
+
+/*
+ * 1- Desarrollar un programa que permita cargar 5 nombres de personas y 
+ * sus edades respectivas. Luego de realizar la carga por teclado de todos 
+ * los datos imprimir los nombres de las personas mayores de edad (mayores o iguales a 18 años), 
+ * se tendrá que crear una clase "Persona" que contenga las propiedades "Nombre" y "Edad". 
+ * Utilizar todos los componentes vistos hasta la fecha (bucles, funciones, clases).
+ */
