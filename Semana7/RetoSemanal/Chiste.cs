cc352009f967e9dea2cc160fc63506e344a1b061
pqmad <3062932420313@ingenieria.usac.edu.gt>
2023-06-16 09:50:53 -0600

    se finalizó reto semanal 7

---
+++ Semana7/RetoSemanal/Chiste.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using static System.Console;
+
+namespace RetoSemanal
+{
+	public class Chiste
+	{
+        private const string _rutaArchivo = "jokes.txt";
+        public string id { get; set; }
+        public string value { get; set; }
+
+        public async Task ObtenerChiste()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string apiUrl = "https://api.chucknorris.io/jokes/random";
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jokeJson = await response.Content.ReadAsStringAsync();
+                    id = JsonConvert.DeserializeObject<Chiste>(jokeJson).id;
+                    value = JsonConvert.DeserializeObject<Chiste>(jokeJson).value;
+
+                    if (!Busqueda(id, false))
+                    {
+                        GuardarDatos();
+                    }
+                    else
+                    {
+                        WriteLine("El chiste ya existe en el archivo.");
+                    }
+                }
+                else
+                {
+                    WriteLine("No se pudo obtener un chiste. Error en la API.");
+                }
+            }
+        }
+
+        public void GuardarDatos()
+        {
+            var cadena = $"{id}#{value}/";
+            try
+            {
+                FileStream archivojugadores;
+                if (File.Exists(_rutaArchivo))
+                {
+                    archivojugadores = new FileStream(_rutaArchivo, FileMode.Append);
+                }
+                else
+                {
+                    archivojugadores = new FileStream(_rutaArchivo, FileMode.Create);
+                }
+                archivojugadores.Write(ASCIIEncoding.ASCII.GetBytes(cadena), 0, cadena.Length);
+                archivojugadores.Close();
+
+                Console.WriteLine("El chiste se ha GUARDADO correctamente.");
+            }
+            catch (Exception ex)
+            {
+                WriteLine("No se pudo guardar el chiste en el archivo");
+            }
+        }
+
+        public bool Busqueda(String aBuscar, bool busquedaCliente)
+        {
+            var encontróChisteCliente = false;
+            if (File.Exists(_rutaArchivo))
+            {
+                FileStream archivo;
+                byte[] contenido = new byte[100000];
+
+                archivo = new FileStream(_rutaArchivo, FileMode.Open);
+                archivo.Read(contenido, 0, (int)archivo.Length);
+                archivo.Close();
+
+                var texto = ASCIIEncoding.ASCII.GetString(contenido).Split("/");
+                
+                foreach (var chiste in texto)
+                {
+                    var datosChiste = chiste.Split("#");
+                    if(datosChiste.Length > 1)
+                    {
+                        if (!busquedaCliente && datosChiste[0].Equals(aBuscar))
+                        {
+                            return true;
+                        }
+                        else if (busquedaCliente && datosChiste[1].ToLower().Contains(aBuscar.ToLower()))
+                        {
+                            WriteLine("-----------------------------");
+                            WriteLine($"ID:  {datosChiste[0]}");
+                            WriteLine($"CHISTE:  {datosChiste[1]}");
+                            WriteLine("-----------------------------");
+                            encontróChisteCliente = true;
+                        }
+                    }
+                    
+                }
+               
+            }
+            return encontróChisteCliente;
+        }
+
+
+    }
+}
+
---
+++ Semana7/RetoSemanal/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Console;
+
+
+namespace RetoSemanal;
+class Program
+{
+    
+
+    static async Task Main(string[] args)
+    {
+        var opcion = "0"; 
+        while (!opcion.Contains("3"))
+        {
+            WriteLine("1. Obtener chiste aleatorio");
+            WriteLine("2. Buscar chistes por palabra clave");
+            WriteLine("3. Salir");
+            WriteLine("Ingrese la opción deseada: ");
+            opcion = ReadLine();
+
+            Chiste chiste = new Chiste();
+            switch (opcion)
+            {
+                case "1":
+                    await chiste.ObtenerChiste();
+                    break;
+                case "2":
+                    WriteLine("Ingrese la palabra clave: ");
+                    string palabraClave = ReadLine();
+                    if (!chiste.Busqueda(palabraClave, true))
+                    {
+                        WriteLine("No se encontro la palabra en ningún chiste");
+                    }
+                    break;
+                case "3":
+                    break;
+                default:
+                    WriteLine("Opción inválida. Inténtelo nuevamente.");
+                    break;
+            }
+
+        }
+    }
+
+
+
+    
+}
+
